f999396596594a8f1ef55e99efb9b5130d499ce4
Pedro David Garcia Lopez <4103404@alu.murciaeduca.es>
2024-10-14 12:55:02 +0200

    mas cambios para comprobar

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ME CAGO EN MI CARA</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>ME CAGO EN MI CARA, O NO?</p>
       </header>
     </div>
   );
